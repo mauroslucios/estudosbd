7a9248794a90978b27709119ab48d3f7417720f2
mauroslucios <mauroslucios@gmail.com>
2023-10-25 21:17:39 -0300

    criado pessoas.sql

---
+++ pessoas.sql
@@ -0,0 +1,14 @@
+insert into pessoa values(null,'amazon web servie'),
+(null,'itau unibanco'),
+(null,'accenture'),
+(null,'mauro lucio'),
+(null,'matheus');
+
+insert into pessoa_fisica values
+(null,'0300817241','mauroslucios',1969-11-30,4),
+(null,'0300817456','lotar matheus',1969-11-30,5);
+
+insert into pessoa_juridica values
+(null,'0123456789/0001-4','aws',1),
+(null,'0123456789/0001-5','itau'),
+(null,'0123456789/0001-6','accenture');
